7bc5486cc3e6faddbcd419fd8c30f8f51218e5a7
unknown <Igor>
2020-05-20 01:14:05 +0300

    Edit documentation strings

--- main.py
+++ main.py
@@ -1,60 +1,82 @@
+import glob
 import dlib
-import sys
 from skimage import io
-from os.path import basename
 from scipy.spatial.distance import euclidean
-
+from os.path import basename
+import re
+import timeit
+import sys
 
 sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
 detector = dlib.get_frontal_face_detector()
-rects = []
-shapes=[]
-descriptors=[]
 
 def open_image_web(pass_to_photo):
-    ''''''
-    name_image = []
+    '''Function open image of web camera
+    input: path to webcam photo
+    output: webcam photo'''
     image_photo_web = io.imread(pass_to_photo)
+    return image_photo_web
+
+def get_name_image(pass_to_photo):
+    '''The function determines the path to the webcam
+    input:path to webcam photo
+    otput:photo name''' 
+    name_image_web = []
     name = basename(pass_to_photo)
-    name_image.append(name)
-    return image_photo_web, name_image_web
+    name_image_web.append(name)
+    return name_image_web
 
-def open_image_passport(pass_to_passport):
-    ''''''
-    image_photo_passport = io.imread(pass_to_photo)
-    return image_photo_passport, name_image_passport
-
-def create_list_passes(pass_to_passport):
+def create_list_passes(pass_to_passports):
+    '''The function creates a list of found paths to photos with passports
+    input:path to passports photo
+    output: passes list'''
     name_photo_file = (glob.glob('{0}/*.jpg'.format(pass_to_passport)))
     list_passes=[i.replace('\\', '/') for i in name_photo_file]
     return list_passes
 
 def search_pass_to_passport(list_passes, name_image):
-    i = ''
-    for i in list_passes:
-        text = i
+    '''The function searches for the path to the passport 
+        photo whose name matches the name of the photo from the web camera
+        input: passes list, name image web camera
+        output: pass to passport image'''
+    pass_list = ''
+    for pass_list in list_passes:
+        text = pass_list
         for j in name_image:
             find = re.search(j, text)
             if find:
-                pass_to_passport=i
-                return pass_to_passport
+                pass_to_passports=pass_list
+                return pass_to_passports
 
+def open_image_passport(pass_to_passports):
+    '''Function open image of passport
+    input: path to passport photo
+    output: passport photo'''
+    image_photo_passport = io.imread(pass_to_passports)
+    return image_photo_passport
 
-def list_image(name_image_web, name_image_passport):
-
-
-def search_descriptors(rects, shapes, descriptors):
+def comparison_photo(pass_to_photo, pass_to_passport):
+    
+    list_names = []
+    rectangle_face = []
+    shapes=[]
+    descriptors=[]
+    list_names.append(pass_to_photo)
+    list_names.append(pass_to_passport)
     for photo in list_names:
+        image_photo = io.imread(photo)
         detect_photo = detector(image_photo, 1)
-        rects.append(detect_photo[0])
+        rectangle_face.append(detect_photo[0])
         shape = sp(image_photo, detect_photo[0])
         shapes.append(shape)
         face_desc = facerec.compute_face_descriptor(image_photo, shape)
-        euclidean_distance = descriptors.append(face_desc)
+        descriptors.append(face_desc)
+    return rectangle_face, shapes, descriptors
 
-        return euclidean_distance
-
+def calculate_euclidean(descriptors):
+    euclidean_distance=euclidean(descriptors[0], descriptors[1])
+    return euclidean_distance
 
 def decision_making(euclidean_distance):
     if euclidean_distance < 0.6:
@@ -62,24 +84,17 @@ def decision_making(euclidean_distance):
     else:
         return print("Verification not Passed !!!!")
 
-
-
-
-def main_algoritm(pass_to_photo, key, path):
+def main_algoritm(**kwargs):
     """main algoritm"""
-    image, photo = open_image(pass_to_photo, pass_to_passport)
-
-
-
-
-    if key == 'run':
-        cv2.imwrite(path_to_save, image_with_contours)  # run and get result
-    elif key == 'save':
-                                                        # save descriptors
-    else key = 'time':
-
-
-
+    image_photo_web = open_image_web(pass_to_photo)
+    name_image_web=get_name_image(pass_to_photo)
+    list_passes=create_list_passes(pass_to_photo)
+    pass_to_passports=search_pass_to_passport(list_passes, name_image_web)
+    image_photo_passport=open_image_passport(pass_to_passports)
+    rectangle_face, shapes, descriptors=comparison_photo(pass_to_photo, pass_to_passports)
+    decision_making(calculate_euclidean(descriptors))
+    print("function execution time is - " + str(timeit.timeit( "main_algoritm", 
+    setup="from __main__ import main_algoritm", number=1)))
 
 if __name__ == "__main__":
     
@@ -87,23 +102,13 @@ if __name__ == "__main__":
 
     try:
         pass_to_photo =  sys.argv[1]
-        pass_to_passport = 'data_base/passports'
-        path_to_save 
-        main_algoritm()
-
-        key = sys.argv[2]
-            if key not in ['run', 'save', 'time']:
-                assert False, 'Error'
-            if key == 'o':
-                try:
-                    path_to_save = 'data_base/save/out.jpg'
-                except:
-                    print('Error')
-
-
+        pass_to_passport = 'data_base/passports' 
+        
     except Exception as error:
         print(error)
         raise AttributeError
+
+    main_algoritm()
         
 
     
